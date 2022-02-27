2b0f6f2724fa58e0c0f24950880d9f48e2813cac
pientka.k <pientka.k@gmail.com>
2022-02-27 08:20:09 +0100

    added persistance di extensions

---
+++ Olo.Persistance/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Olo.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OloDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OpolskaLigaOrlika")));
+
+            services.AddScoped<OloDbContext>(provider => provider.GetService<OloDbContext>());
+
+            return services;
+        }
+    }
+}
