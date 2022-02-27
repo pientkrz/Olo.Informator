faa3d91488866f014fc8a1f55a1d1af0be87a134
pientka.k <pientka.k@gmail.com>
2022-02-27 08:19:55 +0100

    added dbcontext

---
+++ Olo.Informator/Interfaces/IOloDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Olo.Domain.Entities;
+
+namespace Olo.Informator.Interfaces
+{
+    public interface IOloDbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<MatchHistory> MatchHistories { get; set; }
+    }
+}
---
+++ Olo.Persistance/OloDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Olo.Common;
+using Olo.Domain.Entities;
+using Olo.Informator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olo.Persistance
+{
+    public class OloDbContext : DbContext, IOloDbContext
+    {
+        private readonly IDateTime _dateTime;
+
+        public OloDbContext(DbContextOptions<OloDbContext> options)
+            : base(options)
+        {
+        }
+
+        public OloDbContext(
+            DbContextOptions<OloDbContext> options,
+            IDateTime dateTime)
+            : base(options)
+        {
+            _dateTime = dateTime;
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<MatchHistory> MatchHistories { get; set; }
+    }
+}
